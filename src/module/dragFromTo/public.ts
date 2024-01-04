902f803db66c0d674b557dadbe69ac2138cd67da
stacew <chunrans4@naver.com>
2024-01-04 19:47:58 +0900

    chore: cleanup dragFromTo

--- src/module/dragFromTo/public.ts
+++
@@ -1,9 +0,0 @@
-export class DftCustomEvent {
-  static SUCCESS = "DRAG_CUSTOM_EVENT_SUCCESS";
-  static PREVIEW = "DRAG_CUSTOM_EVENT_PREVIEW";
-  static CANCEL = "DRAG_CUSTOM_EVENT_CANCEL";
-}
-
-export interface DftSkipCondCallback {
-  (element: HTMLElement): boolean;
-}
